fc88e88f6b8715f3bc6c6ffbec99121662bdda4f
prozip <quachkimson2000@gmail.com>
2021-03-17 22:33:31 +0700

    simple draw

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ class Player {
     }
     draw() {
         c.beginPath()
-        c.arc(this.x, this.y, this.radius, 0 , Math.PI *2, false)
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         c.fillStyle = this.color
         c.fill()
     }
 }
 
 class Projectile {
-    constructor(x,y, radius, color, velocity){
+    constructor(x, y, radius, color, velocity) {
         this.x = x
         this.y = y
         this.radius = radius
@@ -30,21 +30,46 @@ class Projectile {
     }
     draw() {
         c.beginPath()
-        c.arc(this.x, this.y, this.radius, 0 , Math.PI *2, false)
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
         c.fillStyle = this.color
         c.fill()
+    }
+    update() {
+        this.draw()
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
     }
 }
 
 const x = canvas.width / 2
-const y = canvas.height /2
+const y = canvas.height / 2
 
 const player = new Player(x, y, 30, 'blue')
 player.draw()
 
-window.addEventListener('click',(event)=>{
-    const projectile = new Projectile(
-        event.clientX, event.clientY, 5, 'red', null
-    )
-    projectile.draw()
+const projectile = new Projectile(
+    canvas.width / 2, canvas.height / 2, 5, 'red', { x: 1, y: 1 }
+)
+
+const projectiles = []
+
+function animate() {
+    requestAnimationFrame(animate)
+    projectiles.forEach((projectile)=>{
+        projectile.update()
+    })
+}
+
+window.addEventListener('click', (event) => {
+    const angle = Math.atan2(event.clientY - canvas.height /2, 
+        event.clientX - canvas.width /2)
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
+    projectiles.push(new Projectile(
+        canvas.width/2, canvas.height/2, 5, 'red', velocity
+    ))
 })
+
+animate()
