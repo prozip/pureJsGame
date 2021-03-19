9c4c624acbec01470e6e13cef8db96014c886f11
prozip <quachkimson2000@gmail.com>
2021-03-19 17:20:44 +0700

    update

--- index.js
+++ index.js
@@ -64,13 +64,9 @@ class Enemy {
 const x = canvas.width / 2
 const y = canvas.height / 2
 
-const player = new Player(x, y, 30, 'blue')
+const player = new Player(x, y, 20, 'blue')
 const projectiles = []
 const enemys = []
-
-const projectile = new Projectile(
-    canvas.width / 2, canvas.height / 2, 5, 'red', { x: 1, y: 1 }
-)
 
 function spawnEnemies() {
     setInterval(() => {
@@ -83,9 +79,9 @@ function spawnEnemies() {
             x = Math.random() * canvas.width
             y = Math.random() * 0.5 ? 0 - radius : canvas.height + radius
         }
-        const color = 'green'
-        const angle = Math.atan2(canvas.height / 2 - y,
-            canvas.width / 2 - x)
+        const color = `hsl(${Math.random() * 360}, 50%, 50%)`
+        const angle = Math.atan2(player.y - y,
+            player.x - x)
         const velocity = {
             x: Math.cos(angle),
             y: Math.sin(angle)
@@ -94,25 +90,69 @@ function spawnEnemies() {
     }, 1000)
 }
 
+let animateID
 function animate() {
-    requestAnimationFrame(animate)
-    c.clearRect(0, 0, canvas.width, canvas.height)
+    animateID = requestAnimationFrame(animate)
+    c.fillStyle = 'rgba(0,0,0,0.1)'
+    c.fillRect(0, 0, canvas.width, canvas.height)
     player.draw()
-    projectiles.forEach((projectile) => {
+
+    projectiles.forEach((projectile, index) => {
         projectile.update()
+        // remove from edge of screen
+        if (projectile.x + projectile.radius < 0 ||
+            projectile.x - projectile.radius > canvas.width ||
+            projectile.y + projectile.radius < 0 ||
+            projectile.y - projectile.radius > canvas.height) {
+            setTimeout(() => {
+                projectiles.splice(index, 1)
+            }, 0);
+        }
     })
     enemys.forEach((enemy, eIndex) => {
         enemy.update()
+
+        //end game
+        const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+        if (dist - enemy.radius - player.radius < 0.01) {
+            if (player.radius - enemy.radius / 3 < 10) {
+                cancelAnimationFrame(animateID)
+            } else {
+                gsap.to(player, {
+                    radius: player.radius - Math.round(enemy.radius / 3)
+                })
+                setTimeout(() => {
+                    enemys.splice(eIndex, 1)
+                }, 0);
+            }
+        }
+
+
+
         projectiles.forEach((projectile, pIndex) => {
             const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
 
-            // objects touch
-
+            // projectile touch enemy
             if (dist - enemy.radius - projectile.radius < 1) {
-                setTimeout(() => {
-                    enemys.splice(eIndex, 1)
-                    projectiles.splice(pIndex, 1)
-                }, 0);
+
+
+                if (enemy.radius - 10 > 10) {
+                    gsap.to(enemy, {
+                        radius: enemy.radius - 10
+                    })
+                    gsap.to(player, {
+                        radius: player.radius + 1.25
+                    })
+                    setTimeout(() => {
+                        projectiles.splice(pIndex, 1)
+                    }, 0);
+                } else {
+                    player.radius += Math.round(enemy.radius / 8)
+                    setTimeout(() => {
+                        enemys.splice(eIndex, 1)
+                        projectiles.splice(pIndex, 1)
+                    }, 0);
+                }
             }
         })
     })
@@ -122,8 +162,8 @@ window.addEventListener('click', (event) => {
     const angle = Math.atan2(event.clientY - player.y,
         event.clientX - player.x)
     const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle)
+        x: Math.cos(angle) * 2,
+        y: Math.sin(angle) * 2
     }
 
     projectiles.push(new Projectile(
@@ -131,19 +171,31 @@ window.addEventListener('click', (event) => {
     ))
 })
 window.addEventListener('keydown', function (event) {
-    switch (event.key) {
-        case 'w':
-            player.y = player.y - 10
-            break;
-        case 'a':
-            player.x = player.x - 10
-            break;
-        case 's':
-            player.y = player.y + 10
-            break;
-        case 'd':
-            player.x = player.x + 10
-            break;
+    // switch (event.key) {
+    //     case 'w':
+    //         player.y = player.y - 10
+    //         break;
+    //     case 'a':
+    //         player.x = player.x - 10
+    //         break;
+    //     case 's':
+    //         player.y = player.y + 10
+    //         break;
+    //     case 'd':
+    //         player.x = player.x + 10
+    //         break;
+    // }
+    if (event.key === 'w') {
+        player.y = player.y - 10
+    }
+    if (event.key === 'a') {
+        player.x = player.x - 10
+    }
+    if (event.key === 's') {
+        player.y = player.y + 10
+    }
+    if (event.key === 'd') {
+        player.x = player.x + 10
     }
 })
 animate()
